4b1dc0b24f9af10f50c09770ae4033c9ff75987b
Diogo Ferreira <diogodsferreira@gmail.com>
2023-06-09 15:12:39 +0100

    Delete sensor_repository and update services

--- sensor_repository/config/test.exs
+++
@@ -1,8 +0,0 @@
-import Config
-
-config :sensor_repository,
-  producer_module: Broadway.DummyProducer
-
-config :sensor_repository, SensorMessage.Repo,
-  database: "sensor_repository_repo_testing",
-  pool: Ecto.Adapters.SQL.Sandbox
