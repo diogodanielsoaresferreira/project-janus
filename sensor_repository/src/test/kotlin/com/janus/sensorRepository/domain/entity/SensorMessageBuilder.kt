d482ca673513fc5d7c31ac836ca0585a1385a795
Diogo Ferreira <diogodsferreira@gmail.com>
2024-01-28 17:27:12 +0100

    Fix tests

--- sensor_repository/src/test/kotlin/com/janus/sensorRepository/domain/entity/SensorMessageBuilder.kt
+++ sensor_repository/src/test/kotlin/com/janus/sensorRepository/domain/entity/SensorMessageBuilder.kt
@@ -4,7 +4,7 @@ import java.time.OffsetDateTime
 import java.time.ZoneOffset
 
 data class SensorMessageBuilder (
-    val sensorId: String = "sensorId2",
+    val sensorId: String = "sensorId",
     val timestamp: OffsetDateTime = OffsetDateTime.of(2023, 1, 1, 0, 0, 0, 0, ZoneOffset.UTC),
     val value: String = "value",
     val valueType: ValueType = ValueType.STRING
